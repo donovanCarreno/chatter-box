f09a0d0a0b7ed29963fb6b360689e0ebe9bad731
donovanCarreno <donovan_carreno@yahoo.com>
2019-07-21 16:51:17 -0500

    feat(routes): add signup route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,34 @@
-exports.signup = (req, res, next) => {
-  const {username, password} = req.body
-  res.status(200).json({username, password})
+const bcrypt = require('bcryptjs')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
+
+const User = require('../models/user')
+
+exports.signup = async (req, res, next) => {
+  const {email, username, password} = req.body
+  const existingUser = await User.findOne({
+    where: {
+      [Op.or]: [
+        {email},
+        {username}
+      ]
+    }
+  })
+
+  if (existingUser) {
+    return res.status(422).json({message: 'email/username already exists'})
+  }
+
+  bcrypt.genSalt(10, (error, salt) => {
+    bcrypt.hash(password, salt, async (err, hashedPwd) => {
+      if (err) throw err
+      // await User.sync({force: true})
+      const user = await User.create({
+        email,
+        username,
+        password: hashedPwd
+      })
+      res.status(201).json({message: 'user created', user})
+    })
+  })
 }
