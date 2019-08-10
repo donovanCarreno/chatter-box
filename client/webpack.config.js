b6216acf1b42c2d272db2dab7de20c1b89667488
donovanCarreno <donovan_carreno@yahoo.com>
2019-08-10 09:14:52 -0500

    feat(client): react

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = function(env, argv) {
+  console.log({env})
+  return {
+    mode: env.production ? 'production' : 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    devtool: env.production ? 'cheap-module-source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      hot: true,
+      open: true,
+      overlay: {
+        warnings: true,
+        errors: true
+      },
+      proxy: {
+        '/api': {
+          target: 'htp://localhost:5006',
+          pathRewrite:{'^/api' : ''}
+        }
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          loader: 'babel-loader'
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+    ]
+  }
+}
